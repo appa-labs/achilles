24d208bcb91dbad5bdb568e3321bdbb75c3b9495
dedalusmoonlight <tim.tagil@mail.ru>
2024-02-23 15:22:51 +0300

    sfml-static

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,22 @@
 #include <SFML/Graphics.hpp>
 
 int main() {
-	sf::RenderWindow window(sf::VideoMode(800, 600), "Achilles test");
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
-	sf::Event event;
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
 
-	while (window.isOpen()) {
-		while (window.pollEvent(event)) {
-			if (event.type == sf::Event::Closed) {
-				window.close();
-			}
-		}
-	}
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
 
-	return 0;
+    return 0;
 }
