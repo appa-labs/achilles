46128ae82f7ada507f01e2a9819f0905c43f8efd
dedalusmoonlight <tim.tagil@mail.ru>
2024-03-05 23:03:38 +0300

    debug files -> debug inplace

--- libs/Engine/include/tools.h
+++ libs/Engine/include/tools.h
@@ -1,10 +1,10 @@
 #pragma once
 
+#include <SFML/Graphics.hpp>
 #include <cmath>
 #include <string>
 #include <utility>
 #include <vector>
-
 extern inline const double PH_CONST_G = 9.81;
 extern inline const double PH_CONST_COLLISION_PRES = 0.15;
 
@@ -134,4 +134,44 @@ class MoveableObject : public Object {
     explicit MoveableObject(const vec& basepoint, const std::vector<line>& polygons, double m)
         : Object(basepoint, polygons), mass(m) {
     }
+};
+
+class ColliderShape : public sf::Shape {
+   public:
+    ColliderShape() : points_{{0, 0}, {0, 0}}, point_count_(50), radius_(PH_CONST_COLLISION_PRES) {
+        update();
+    }
+
+    void setPoints(const std::pair<sf::Vector2f, sf::Vector2f>&);
+
+    void setRadius(const int&);
+
+    void setPointCount(const size_t&);
+
+    [[nodiscard]] virtual size_t getPointCount() const override {
+        return point_count_;
+    }
+
+    [[nodiscard]] virtual sf::Vector2f getPoint(size_t index) const override {
+        static const float pi = 3.141592654f;
+        static const float k_eps = 1e-9;
+
+        const float segment_normal_angle =
+            pi / 2 -
+            std::atan2(points_.first.y - points_.first.y, points_.first.x - points_.first.x);
+        const float angle = index * 2 * pi / getPointCount();
+
+        const float x = std::cos(angle + segment_normal_angle) * radius_;
+        const float y = std::sin(angle + segment_normal_angle) * radius_;
+
+        if (angle - pi < k_eps) {
+            return {points_.first.x + x, points_.first.y + y};
+        }
+        return {points_.second.y + x, points_.second.y + y};
+    }
+
+   private:
+    std::pair<sf::Vector2f, sf::Vector2f> points_;
+    size_t point_count_;
+    int radius_;
 };
--- libs/Engine/src/tools.cpp
+++ libs/Engine/src/tools.cpp
@@ -108,3 +108,18 @@ void MoveableObject::SumNormalForces(Object* obj) {
     }
     fout << std::endl;
 }
+
+void ColliderShape::setPoints(const std::pair<sf::Vector2f, sf::Vector2f>& points) {
+    points_ = points;
+    update();
+}
+
+void ColliderShape::setRadius(const int& radius) {
+    radius_ = radius;
+    update();
+}
+
+void ColliderShape::setPointCount(const size_t& point_count) {
+    point_count_ = point_count;
+    update();
+}
