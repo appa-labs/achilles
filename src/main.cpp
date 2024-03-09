8713c404d6bfe4827f4fadc484244096bd74a217
WhyNovaa <novikov.arsenij@gmail.com>
2024-03-09 20:35:05 +0300

    [fix] delta time fix

--- src/main.cpp
+++ src/main.cpp
@@ -11,10 +11,11 @@ int main() {
     spdlog::info("Engine start");
     engine.initialization();
     spdlog::info("Engine initialization");
-
+    sf::Clock deltaClock;
     while (engine.window.isOpen()) {
         auto start = std::chrono::system_clock::now();
         sf::Event event;
+        sf::Time dt = deltaClock.restart();
         while (engine.window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
                 engine.window.close();
@@ -47,9 +48,7 @@ int main() {
         engine.physicsPerFrame();
         engine.renderFrame();
 
-        auto end = std::chrono::system_clock::now();
-        engine.frametime =
-            std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() / 1000.;
+        engine.frametime = dt.asMilliseconds() / 1000.;
     }
 
     spdlog::info("Engine stops with code {}", 0);
